481bb7aa7c169b1e5bf320bb11433094b44da6c7
jcsequeira <joao.sequeira1989@gmail.com>
2023-11-25 10:07:11 +0000

    Limpeza de imports a mais sintetizaçao dos imports usando o * RunServerAPI:lowercase API->api, objectos do artista sintetizados num metodo artistaInit.

--- RestAPI/src/main/java/RunServerAPI.java
+++ RestAPI/src/main/java/RunServerAPI.java
@@ -1,9 +1,8 @@
 
 import com.google.gson.Gson;
-import controller.ArtistaController;
-import repository.ArtistRepository;
-import repository.DBConnection;
-import service.ArtistaService;
+import controller.*;
+import repository.*;
+import service.*;
 import spark.Spark;
 
 import static spark.Spark.*;
@@ -11,18 +10,27 @@ import static spark.Spark.*;
 public class RunServerAPI {
 
     public static void main(String[] args) {
+        //endpoint de teste
+        get("/hello", (req, res) -> "Hello World");
 
+        //endpoints Artista
+        ArtistaController artistaInit = artistaInit();
+        get("/api/artista/all", (req, res) -> artistaInit.getAllArtists());
+        //TBD getbyID
+
+
+
+
+
+
+    }
+
+    //carrega recursos para os endpoints do artista
+    public static ArtistaController artistaInit() {
         ArtistRepository artistRepository = new ArtistRepository(DBConnection.getConnection());
         ArtistaService artistaService = new ArtistaService(artistRepository);
         Gson gson = new Gson();
-        ArtistaController artistaController = new ArtistaController(artistaService, gson);
-
-
-        get("/hello", (req, res) -> "Hello World");
-
-        get("/API/artista/all", (req, res) -> artistaController.getAllArtists());
-
-    }
+        return new ArtistaController(artistaService, gson);}
 
 
 }
--- RestAPI/src/main/java/controller/ArtistaController.java
+++ RestAPI/src/main/java/controller/ArtistaController.java
@@ -4,12 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import controller.adapters.LocalDateAdapter;
 import model.Artista;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Response;
 import service.ArtistaService;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArtistaController {
