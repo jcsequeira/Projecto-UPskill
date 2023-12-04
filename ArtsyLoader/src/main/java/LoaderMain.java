3715644a562e50f7c5b94795f04fdaaa82d1f6c2
jcsequeira <joao.sequeira1989@gmail.com>
2023-12-04 17:23:38 +0000

    testes

--- ArtsyLoader/src/main/java/LoaderMain.java
+++ ArtsyLoader/src/main/java/LoaderMain.java
@@ -1,9 +1,14 @@
 
 
+import Utils.Utils;
 import com.opencsv.exceptions.CsvException;
 import controller.Controller;
+import dataprocessorservice.DataProcessor;
+import model.Cidade;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static controller.Controller.populateCidades;
 
@@ -13,10 +18,12 @@ public class LoaderMain {
 
     public static void main(String[] args) throws IOException, CsvException {
 
-        populateCidades();
+        //populateCidades();
 
-        Controller.populateArtistas();
+        //Controller.populateArtistas();
 
+        Map<Integer, String> cityMap = Utils.createCityMap(Utils.readCityNamesFromCsv("ArtsyLoader/src/main/resources/citysPT.csv"));
+        System.out.println(cityMap);
 
 
 
