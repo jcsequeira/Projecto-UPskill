562895a372369d6da7bdbbd5b7023c990ac7acff
LRossa13 <luisrossa1@hotmail.com>
2023-12-07 12:31:23 +0000

    Tabela Galeria & Evento (controller)

--- ArtsyLoader/src/main/java/controller/Controller.java
+++ ArtsyLoader/src/main/java/controller/Controller.java
@@ -3,9 +3,7 @@ package controller;
 
 import Utils.Utils;
 import apiserviceartsy.ApiServiceArtsy;
-import artsymodel.ArtsyArtist;
-import artsymodel.ArtsyArtwork;
-import artsymodel.ArtsyGene;
+import artsymodel.*;
 import com.opencsv.exceptions.CsvException;
 import dataprocessorservice.*;
 import restapiservice.RestApiService;
@@ -21,6 +19,8 @@ public class Controller {
     private static final String ALL_ARTIST_ARTSY_URL = "https://api.artsy.net/api/artists?artworks=true&sort=-trending&size=1500&page=1";
     private static final String ALL_MOVIMENTOS_ARTSY_URL = "https://api.artsy.net/api/genes?size=1000";
     private static final String ALL_ARTWORKS_ARTSY_URL = "https://api.artsy.net/api/artworks?size=500&page=1";
+    private static final String ALL_PARTNERS_ARTSY_URL = "https://api.artsy.net/api/partners";
+    private static final String ALL_SHOWS_ARTSY_URL =  "https://api.artsy.net/api/shows?status=upcoming";
     private static final String REST_ENDPOINT_CIDADES_API_URL="http://localhost:4567/populate/cidades";
     private static final String REST_ENDPOINT_PAISES_API_URL="http://localhost:4567/populate/paises";
     private static final String REST_ENDPOINT_ARTISTAS_API_URL="http://localhost:4567/populate/artistas";
@@ -28,7 +28,8 @@ public class Controller {
     private static final String REST_ENDPOINT_TECNICAS_API_URL="http://localhost:4567/populate/tecnicas";
     private static final String REST_ENDPOINT_MATERIAIS_API_URL="http://localhost:4567/populate/materiais";
     private static final String REST_ENDPOINT_OBRASARTE_API_URL="http://localhost:4567/populate/obrasarte";
-
+    private static final String REST_ENDPOINT_GALERIAS_API_URL = "http://localhost:4567/populate/galerias";
+    private static final String REST_ENDPOINT_EVENTOS_API_URL = "http://localhost:4567/populate/eventos";
 
 
 
@@ -56,6 +57,16 @@ public class Controller {
                 ,DataProcessor.listProcessor(ApiServiceArtsy.getAllArtsyItems(ALL_MOVIMENTOS_ARTSY_URL, ArtsyGene.class), GeneConverter.class));
     }
 
+    public static void populateGalerias() throws IOException {
+        RestApiService.postToRestApi(REST_ENDPOINT_GALERIAS_API_URL
+                ,DataProcessor.listProcessor(ApiServiceArtsy.getAllArtsyItems(ALL_PARTNERS_ARTSY_URL, ArtsyPartner.class), PartnerConverter.class));
+    }
+
+    public static void populateEventos() throws IOException {
+        RestApiService.postToRestApi(REST_ENDPOINT_EVENTOS_API_URL
+                ,DataProcessor.listProcessor(ApiServiceArtsy.getAllArtsyItems(ALL_SHOWS_ARTSY_URL, ArtsyShow.class), ShowConverter.class));
+    }
+
     public static List<ArtsyArtwork> LoadArtsyArtworksList() throws IOException {
        return ApiServiceArtsy.getAllArtsyItems(ALL_ARTWORKS_ARTSY_URL, ArtsyArtwork.class);
     }
@@ -73,12 +84,4 @@ public class Controller {
     public static void populateObrasArte(List<ArtsyArtwork> allartsyArtworkList) throws IOException {
         RestApiService.postToRestApi(REST_ENDPOINT_OBRASARTE_API_URL, DataProcessor.listProcessor(allartsyArtworkList, ArtworkConverter.class));
     }
-
-
-
-
-
-
-
-
 }
