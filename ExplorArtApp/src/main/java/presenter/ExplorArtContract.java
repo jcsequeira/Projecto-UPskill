d33d20f73bed6f13be3d24a9ed7315545adff959
jcsequeira <joao.sequeira1989@gmail.com>
2023-12-14 14:41:01 +0000

    arrumaçao das interfaces

--- ExplorArtApp/src/main/java/presenter/ExplorArtContract.java
+++ ExplorArtApp/src/main/java/presenter/ExplorArtContract.java
@@ -32,33 +32,32 @@ public interface ExplorArtContract {
 
     interface Presenter {
         void exploreArtists() throws IOException;
-
-        void exploreArtworks() throws IOException;
-
-        void exploreEvents() throws IOException;
-
-
-
-        void addArtwork(Obra_Arte obraArte) throws IOException;
-
-        void ativateArtwork();
-        void deativateArtwork();
         void addArtist(Artista artista) throws IOException;
         void updateArtist();
         void removeArtist();
+        //OrbaArte
+        void exploreArtworks() throws IOException;
+        void addArtwork(Obra_Arte obraArte) throws IOException;
+        void ativateArtwork();
+        void deativateArtwork();
+        //Eventos
+        void exploreEvents() throws IOException;
         void addShow(Evento evento) throws IOException;
         void updateShow();
         void removeShow();
-        void addGalerist();
+        //Galerista
+        void addGalerist(Galerista galerista);
         void updateGalerist();
         void removeGalerist();
+        //Galeria
         void addGallery();
         void updateGallery();
         void removeGallery();
+        //Admin
         void importDataFromArtsy();
         void deleteArtsyData();
 
-
+        //View do's MenusActions
         void doArtistDetails(Artista artista);
 
         void doArtworkDetails(Obra_Arte obraArte) throws IOException;
@@ -69,35 +68,29 @@ public interface ExplorArtContract {
 
     interface Model {
         List<Artista> getArtists() throws IOException;
+        Artista getArtistById(int idArtista) throws IOException;
         void addArtist(Artista artista) throws IOException;
-
+        //ObraArte
         List<Obra_Arte> getArtworks() throws IOException;
-
         void addArtwork(Obra_Arte obraArte) throws IOException;
-
+        //Eventos
         List<Evento> getEvents() throws IOException;
         void addShow(Evento evento) throws IOException;
-
+        //Galerias
         List<Galeria> getGalleries() throws IOException;
-
-        List<Galerista> getGallerists() throws IOException;
-
         Galeria getGalleryById(int galleryId) throws IOException;
+        //Galeristas
+        List<Galerista> getGallerists() throws IOException;
+        void addGalerist(Galerista galerista);
 
-        Artista getArtistById(int idArtista) throws IOException;
-
+        //Foreign keys tables Obra ARte
         List<Tecnica> getTechnics() throws IOException;
-
         Tecnica getTechniqueById(int idTecnica) throws IOException;
-
         List<Movimento> getMovement() throws IOException;
-
         Movimento getMovementById(int idMovimento) throws IOException;
-
-
         List<Materiais> getMaterials() throws IOException;
         Materiais getMaterialById(int idMaterial) throws IOException;
-
+        //Utils
         List<Pais> getPaises() throws IOException;
     }
 }
