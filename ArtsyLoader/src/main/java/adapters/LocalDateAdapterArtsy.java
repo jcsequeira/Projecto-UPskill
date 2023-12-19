25f977f9cf9f3e4ad043b015332b2810ac5169ef
jcsequeira <joao.sequeira1989@gmail.com>
2023-12-19 18:17:07 +0000

    cleen up e javadoc
    Added detailed Javadoc comments to LocalDateAdapterArtsy.java

--- ArtsyLoader/src/main/java/adapters/LocalDateAdapterArtsy.java
+++ ArtsyLoader/src/main/java/adapters/LocalDateAdapterArtsy.java
@@ -13,12 +13,22 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
+/**
+ * A Gson {@link TypeAdapter} for serializing and deserializing {@link LocalDate} objects
+ * based on the Artsy API's date formats.
+ */
 public class LocalDateAdapterArtsy extends TypeAdapter<LocalDate> {
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
     private final DateTimeFormatter formatterShows = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
+    /**
+     * Writes a {@link LocalDate} object to JSON using the specified format.
+     *
+     * @param jsonWriter The JSON writer.
+     * @param date       The {@link LocalDate} object to be written.
+     * @throws IOException If an I/O error occurs during writing.
+     */
     @Override
     public void write(JsonWriter jsonWriter, LocalDate date) throws IOException {
         if (date == null) {
@@ -27,6 +37,14 @@ public class LocalDateAdapterArtsy extends TypeAdapter<LocalDate> {
             jsonWriter.value(formatter.format(date));
         }
     }
+
+    /**
+     * Reads a {@link LocalDate} object from JSON using the specified format.
+     *
+     * @param jsonReader The JSON reader.
+     * @return The deserialized {@link LocalDate} object.
+     * @throws IOException If an I/O error occurs during reading.
+     */
 
     @Override
     public LocalDate read(JsonReader jsonReader) throws IOException {
@@ -42,7 +60,14 @@ public class LocalDateAdapterArtsy extends TypeAdapter<LocalDate> {
             }
         }
     }
-
+    /**
+     * Reads a {@link LocalDate} object from JSON using various parsing strategies.
+     * <p>
+     * If the date string is empty or cannot be parsed, returns {@code null}.
+     *
+     * @param dateString The date string to be parsed.
+     * @return The parsed {@link LocalDate} object or {@code null} if parsing fails.
+     */
     private LocalDate parseDate(String dateString) {
         try {
             return parseWithOffsetDateTime(dateString);
@@ -84,17 +109,35 @@ public class LocalDateAdapterArtsy extends TypeAdapter<LocalDate> {
         }
     }
 
+    /**
+     * Parses a date string using a specific range formatter.
+     *
+     * @param dateString The date string to be parsed.
+     * @return The parsed {@link LocalDate} object.
+     */
     private LocalDate parseWithRangeFormatter(String dateString) {
         DateTimeFormatter rangeFormatter = DateTimeFormatter.ofPattern("'ca.' yyyy/MM", Locale.ENGLISH);
         YearMonth yearMonth = YearMonth.parse(dateString, rangeFormatter);
         return yearMonth.atDay(1);
     }
 
+    /**
+     * Parses a date string using a custom formatter.
+     *
+     * @param dateString The date string to be parsed.
+     * @return The parsed {@link LocalDate} object.
+     */
     private LocalDate parseWithCustomFormatter(String dateString) {
         DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH);
         return LocalDate.parse(dateString, customFormatter);
     }
 
+    /**
+     * Parses a date string using an {@link OffsetDateTime} formatter.
+     *
+     * @param dateString The date string to be parsed.
+     * @return The parsed {@link LocalDate} object.
+     */
     private LocalDate parseWithOffsetDateTime(String dateString) {
         OffsetDateTime offsetDateTime = OffsetDateTime.parse(dateString, formatterShows);
         return offsetDateTime.toLocalDate();
