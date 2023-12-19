dbb37301c6a64f443e09866db00c8455d5563974
jcsequeira <joao.sequeira1989@gmail.com>
2023-12-19 11:44:20 +0000

    Visualizar galeria: mostra cidade e galerista corretamente e N/A em camppos caso seja null ou vazio

--- ExplorArtApp/src/main/java/view/GalleryDetailsView.java
+++ ExplorArtApp/src/main/java/view/GalleryDetailsView.java
@@ -7,12 +7,14 @@ import javafx.scene.layout.VBox;
 import model.ExplorArtModel;
 import model.Galeria;
 
+import java.io.IOException;
+
 public class GalleryDetailsView extends Parent {
-    public GalleryDetailsView(Galeria galeria) {
+    public GalleryDetailsView(Galeria galeria) throws IOException {
         doLayout(galeria);
     }
 
-    private void doLayout(Galeria galeria) {
+    private void doLayout(Galeria galeria) throws IOException {
         ExplorArtModel model = new ExplorArtModel();
 
         // Create a VBox to hold the details
@@ -21,12 +23,13 @@ public class GalleryDetailsView extends Parent {
 
         // Labels
         Label nomeLabel = new Label("Nome da galeria: " + galeria.getNome_Galeria());
-        Label emailLabel = new Label("Email: " + galeria.getEmail());
-        Label telefoneLabel = new Label("Telefone: " + galeria.getTelefone());
-        Label urlLabel = new Label("Website: " + galeria.getWebsite());
-        Label moradaLabel = new Label("Morada: " + galeria.getMorada());
-        Label cidadeLabel = new Label("Cidade: "+ galeria.getId_Cidade());
-        Label galeristaLabel = new Label("Galerista: " + galeria.getId_colaborador());
+        Label emailLabel = new Label("Email: " + ((galeria.getEmail() == null || galeria.getEmail().isEmpty()) ? "N/A" : galeria.getEmail()));
+        Label telefoneLabel = new Label("Telefone: " + ((galeria.getTelefone() == null || galeria.getTelefone().isEmpty()) ? "N/A" : galeria.getTelefone()));
+        Label urlLabel = new Label("Website: " + ((galeria.getWebsite() == null || galeria.getWebsite().isEmpty()) ? "N/A" : galeria.getWebsite()));
+        Label moradaLabel = new Label("Morada: " + ((galeria.getMorada() == null || galeria.getMorada().isEmpty()) ? "N/A" : galeria.getMorada()));
+
+        Label cidadeLabel = new Label("Cidade: "+ model.getCidadeById(galeria.getId_Cidade()));
+        Label galeristaLabel = new Label("Galerista: " + model.getColaboradorById(galeria.getId_colaborador()));
 
 
         detailsLayout.getChildren().addAll(nomeLabel, galeristaLabel, moradaLabel, cidadeLabel,
